ddc5490f2f6100fd5c4b6587eed7f6224b0ac4f6
lincolnsousa <lincolnreis1810@gmail.com>
2025-08-20 14:36:36 -0300

    Cria uma Entidade no banco de dados apartir do NinjaModel

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,53 @@
+package dev.java10x.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+import java.util.LinkedList;
+// Entity transforma uma classe em uma Entidade do Banco de Dados.
+@Entity
+// Criar uma tabela e definir o seu nome.
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    // Id é usado pra criar um id pra cada elemento da tabela. Ele sempre deve ser colocado acima do Long Id.
+    @Id
+    // É usado para gerar um valor automático para Id. O ENUM strategy Identity cria Id de forma sequencial.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
