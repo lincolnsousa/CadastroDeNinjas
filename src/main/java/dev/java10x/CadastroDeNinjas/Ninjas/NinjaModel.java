2eea93516495d12eb4dfca8bc9c08cafac79f05b
lincolnsousa <lincolnreis1810@gmail.com>
2025-08-27 11:19:52 -0300

    feat: Adiciona o MissoesRepository, NinjaService, NinjaRepository e altera o Ninja Model

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -19,14 +19,21 @@ public class NinjaModel {
     @Id
     // É usado para gerar um valor automático para Id. O ENUM strategy Identity cria Id de forma sequencial.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_do_ninja") // Usado para definir o nome da nossa coluna no Banco de Dados
     private Long id;
 
+    @Column(name = "nome_do_ninja")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade")
     private int idade;
+
     // Múltiplas funcionalidades para uma variável.
     @ManyToOne
     // Juntar colunas
