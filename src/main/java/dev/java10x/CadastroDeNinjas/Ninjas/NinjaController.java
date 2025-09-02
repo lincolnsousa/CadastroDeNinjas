3f5dcf4a903b96342710ea354e834ba2275f5963
lincolnsousa <lincolnreis1810@gmail.com>
2025-09-02 18:39:08 -0300

    feat: Adiciona os endpoints: criar, todos, ninjaid, alterar, deletar

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,36 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVinda() {
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    // Adicionar Ninjas
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // Mostrar todos os Ninjas
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar Ninjas";
+    }
+
+    // Mostrar Ninjas por Id
+    @GetMapping("/ninjaid")
+    public String mostrarNinjasPorId() {
+        return "Mostrar Ninja por Id";
+    }
+
+    // Alterar dados dos Ninjas
+    @PutMapping("/alterar")
+    public String alterarDdadosNinja() {
+        return "Alterar dados do Ninja";
+    }
+
+    // Deletar Ninjas por ID
+    @DeleteMapping("/deletar")
+    public String deletarNinja() {
+        return "Ninja deletado";
     }
 
 }
